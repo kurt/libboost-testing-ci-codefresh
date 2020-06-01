e83ec23aa7a2bd11fd539773a91a0cd9e96aecf4
kurt_stewart <kurt@humaninmotion.ca>
2020-06-01 11:54:40 -0700

    Bash script to show failed tests and open coverage report

--- run-tests.sh
+++ run-tests.sh
@@ -1,11 +1,12 @@
 # bash
 
 g++ --coverage -o executables/test-run test-cases.c test.c -lboost_unit_test_framework
-./executables/test-run --log_level=all -log_format=JUNIT >test-results/jutest_results -output_format=XML >test-results/ctest_results.xml
-gcov test-cases.c
+./executables/test-run --log_level=all -log_format=JUNIT >test-results/jutest_results -output_format=XML >test-results/ctest_results.xml > /dev/null
+./executables/test-run --log_level=error output_format=HRF >test-results/failed_results.txt
+gcov test-cases.c > /dev/null
 mv *.gcov gcov/
 mv *.gcda gcov/
 mv *.gcno gcov/
-lcov --directory ./gcov/ --capture --output-file lcov/testout
-genhtml lcov/testout --output-directory html
-
+lcov --directory ./gcov/ --capture --output-file lcov/testout > /dev/null
+genhtml lcov/testout --output-directory html > /dev/null
+firefox ./html/index.html
